3ddc1c5a1b2e0d2bad8a1df81f7498ac0ad9cb6f
ValckeMalo <malovalcke@gmail.com>
2024-11-25 14:04:49 +0100

    omg tools projects page add

--- Js/GamePageEvent.js
+++ Js/GamePageEvent.js
@@ -11,6 +11,16 @@ function showSection(section) {
   }
 }
 
+function showUniqueSection(section) {
+  // Cacher les deux sections
+  document.getElementById("overview-section").style.display = "none";
+
+  // Afficher la section demandée
+  if (section === "overview") {
+    document.getElementById("overview-section").style.display = "block";
+  }
+}
+
 window.addEventListener("load", () => {
   document.getElementById("overview-btn").click();
 })
